49e97b44bd9e1a484bdc2fe7bafa5caeb2915bcf
xpenno255 <47892554+xpenno255@users.noreply.github.com>
2019-05-11 08:09:33 +0100

    Add files via upload

---
+++ SWDark/swdark_functions.tcl
@@ -0,0 +1,34 @@
+package provide swdark_functions 1.0
+
+proc horizontal_clicker {bigincrement smallincrement varname minval maxval x y x0 y0 x1 y1} {
+	set xrange [expr {$x1 - $x0}]
+	set xoffset [expr {$x - $x0}]
+	set midpoint [expr {$x0 + ($xrange / 2)}]
+	set onequarterpoint [expr {$x0 + ($xrange / 5)}]
+	set threequarterpoint [expr {$x1 - ($xrange / 5)}]
+	if {[info exists $varname] != 1} {
+		# if the variable doesn't yet exist, initiialize it with a zero value
+		set $varname 0
+	}
+	set currentval [subst \$$varname]
+	set newval $currentval
+	if {$x < $onequarterpoint} {
+		set newval [expr "1.0 * \$$varname - $bigincrement"]
+	} elseif {$x < $midpoint} {
+		set newval [expr "1.0 * \$$varname - $smallincrement"]
+	} elseif {$x < $threequarterpoint} {
+		set newval [expr "1.0 * \$$varname + $smallincrement"]
+	} else {
+		set newval [expr "1.0 * \$$varname + $bigincrement"]
+	}
+	set newval [round_to_two_digits $newval]
+	if {$newval > $maxval} {
+		set $varname $maxval
+	} elseif {$newval < $minval} {
+		set $varname $minval
+	} else {
+		set $varname [round_to_two_digits $newval]
+	}
+	update_onscreen_variables
+	return
+}
